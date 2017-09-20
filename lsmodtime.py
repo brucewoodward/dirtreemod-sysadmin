4c4bafdec97797b96b491033fa3c0786834c055d
Bruce Woodward <bruce.woodward@gmail.com>
2017-09-21 07:59:10 +1000

    Skip over files that don't stat

--- lsmodtime.py
+++ lsmodtime.py
@@ -4,14 +4,18 @@ import os, sys, time
 from stat import *
 
 def walktree(dir, condition):
-	"""walk directory, stating each file; return filename and value from stat"""
+	"""walk directory, stat-ing each file; return filename and value from stat"""
 	results = []
 	for root, dirs, files in os.walk(dir):
 		for f in files:
 			fullpath = os.path.join(root, f)
-			stat = os.stat(fullpath)
-			if condition(fullpath, stat):
-				results.append((fullpath, stat))
+                        try:
+                                stat = os.stat(fullpath)
+                        except OSError as e:
+                                sys.stderr.write("Couldn't stat " + '{:<35}'.format(fullpath) + "\n")
+                        else:
+                                if condition(fullpath, stat):
+                                        results.append((fullpath, stat))
 	return results
 
 if __name__ == '__main__':
